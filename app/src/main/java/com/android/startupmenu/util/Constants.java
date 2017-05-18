9197a9fdcbaf6a154a006eee0b47f7a1a20e1a81
CaoYongren <m18410261910@163.com>
2017-05-18 15:24:57 +0800

    Openthos

---
+++ app/src/main/java/com/android/startupmenu/util/Constants.java
@@ -0,0 +1,34 @@
+package com.android.startupmenu.util;
+
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+
+/**
+ * Created by ljh on 17-5-17.
+ */
+
+public class Constants {
+//    public static final String ACTION_STARTMENU_SEND_SQLITE_INFO = Intent.ACTION_STARTMENU_SEND_SQLITE_INFO;
+//    public static final String APPNAME_OTO_FILEMANAGER = ApplicationInfo.APPNAME_OTO_FILEMANAGER;
+//    public static final String STATUS_BAR_SHOW_SUGGEST = Intent.STATUS_BAR_SHOW_SUGGEST;
+//    public static final String ACTION_OPEN_APPLICATION = Intent.ACTION_OPEN_APPLICATION;
+//    public static final String ACTION_STARTUPMENU_SEND_INFO_LOCK = Intent.ACTION_STARTUPMENU_SEND_INFO_LOCK;
+//    public static final String STARTMENU_UNLOCKED = Intent.STARTMENU_UNLOCKED;
+//    public static final String ACTION_SEND_CLICK_INFO = Intent.ACTION_SEND_CLICK_INFO;
+//
+//
+//    public static final int FLAG_ACTIVITY_RUN_PHONE_MODE = Intent.FLAG_ACTIVITY_RUN_PHONE_MODE;
+//    public static final int FLAG_ACTIVITY_RUN_PC_MODE = Intent.FLAG_ACTIVITY_RUN_PC_MODE;
+
+    public static final String ACTION_STARTMENU_SEND_SQLITE_INFO = "com.android.startupmenu.SQLITE_CHANGE";
+    public static final String APPNAME_OTO_FILEMANAGER = "com.openthos.filemanager";
+    public static final String STATUS_BAR_SHOW_SUGGEST = "android.intent.statusbar.show.suggest";
+    public static final String ACTION_OPEN_APPLICATION = "android.intent.action.OPEN_APPLICATION";
+    public static final String ACTION_STARTUPMENU_SEND_INFO_LOCK = "com.android.action.STARTMENU_INFO_SEND";
+    public static final String STARTMENU_UNLOCKED = "com.android.startmenu.unlock";
+    public static final String ACTION_SEND_CLICK_INFO = "com.android.action.PACKAGE_SEND";
+
+
+    public static final int FLAG_ACTIVITY_RUN_PHONE_MODE = 0x00000800;
+    public static final int FLAG_ACTIVITY_RUN_PC_MODE = 0x00001000;
+}
